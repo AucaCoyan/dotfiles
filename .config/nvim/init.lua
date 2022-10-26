4e4f27cd539b905909c5561743e1e45ab4372a3e
Auca Maillot <aucacoyan@gmail.com>
2022-10-26 00:51:13 -0300

    :sparkles: add colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,8 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
+require "user.colorscheme"
+
+
+-- vim.cmd "colorscheme synthwave84" -- doesnt work
+vim.cmd "colorscheme shades_of_purple"
