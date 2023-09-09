20ee2690ce5685dbd2ed57ce23ca876e7d033c86
Auca Maillot <aucacoyan@gmail.com>
2023-09-09 18:32:39 -0300

    :sparkles: Add lsp for rust

--- .config/preconfigured-nvim/NvChad/custom/configs/lspconfig.lua
+++ .config/preconfigured-nvim/NvChad/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 local util = require("lspconfig/util")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "tsserver", "clangd", "ruff_lsp" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -28,5 +28,12 @@ lspconfig.rust_analyzer.setup({
   }
 })
 
--- 
--- lspconfig.pyright.setup { blabla}
+require'lspconfig'.ruff_lsp.setup{
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+
