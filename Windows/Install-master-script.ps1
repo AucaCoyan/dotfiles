a87f5a49a9f1955ca27cf4da23db109987f95b6b
Auca <aucacoyan@gmail.com>
2023-09-18 15:23:03 -0300

    :sparkles: add `everything` to scoop

--- Windows/Install-master-script.ps1
+++ Windows/Install-master-script.ps1
@@ -108,7 +108,7 @@ try {
     # core
     scoop install 7zip audacity autohotkey azuredatastudio bat
     scoop install broot calibre delta difftastic discord draw.io 
-    scoop install dust fd ffmpeg fzf gcc gh git gitui glab glow googlechrome jpegview-fork
+    scoop install dust everything fd ffmpeg fzf gcc gh git gitui glab glow googlechrome jpegview-fork
     scoop install inkscape insomnia keepassxc mongodb mongodb-compass mongosh neovide neovim
     scoop install nomino nu obsidian oh-my-posh obs-studio peazip postman powertoys psreadline rga 
     scoop install ripgrep rustdesk scoop-completion sublime-merge sumatrapdf teamviewer 
