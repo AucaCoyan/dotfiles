8a38b585d2b490b1954f530740ceb2ce413af584
Auca <aucacoyan@gmail.com>
2023-09-28 18:43:50 -0300

    :sparkles: add nvim ps1 scripts

--- Windows/scripts/empty-LazyVim.ps1
+++ Windows/scripts/empty-LazyVim.ps1
@@ -2,11 +2,9 @@ $ErrorActionPreference = "Stop"
 . "$PSScriptRoot\ps_support.ps1"
 
 
-Write-Output "Install NvChad in the machine"
+Write-Output "Clone empty LazyVim in the machine"
 Write-Output "-----------------------------"
 Write-Output ""
-
-Write-Output "1. Cloning NvChad"
 
 # remove nvim-data
 $hasnNvimDataDirectory = Test-Path "$HOME\AppData\Local\nvim-data"
@@ -22,22 +20,9 @@ if ($hasNvimLuaCustomDirectory) {
     Remove-Item "$HOME\AppData\Local\nvim" -Recurse -Force
 }
 
-Write-Host "`nCloning NvChad" -ForegroundColor White
-git clone https://github.com/NvChad/NvChad $HOME\AppData\Local\nvim --depth 1
+Write-Host "`nCloning LazyVim" -ForegroundColor White
+git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
 
 
-Write-Host "`nMaking the symlinks" -ForegroundColor White
-Write-Output "2. Making the symlinks"
-
-$originPath = "$HOME\AppData\Local\nvim\lua\custom"
-$destinationPath = "$HOME\repos\dotfiles\.config\preconfigured-nvim\NvChad\custom\"
-
-New-Item -ItemType Junction `
-    -Path $originPath `
-    -Target $destinationPath
-
-Write-Output "Job's done!"
-
-## other stuff
-# script to isntall kickstart nvim
-#
+Write-Host "`nRemoving .git folder" -ForegroundColor White
+Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
--- Windows/scripts/nvim.ps1
+++ Windows/scripts/nvim.ps1
@@ -2,11 +2,8 @@ $ErrorActionPreference = "Stop"
 . "$PSScriptRoot\ps_support.ps1"
 
 
-Write-Output "Install NvChad in the machine"
+Write-Output "Install dotfiles-nvim in the machine"
 Write-Output "-----------------------------"
-Write-Output ""
-
-Write-Output "1. Cloning NvChad"
 
 # remove nvim-data
 $hasnNvimDataDirectory = Test-Path "$HOME\AppData\Local\nvim-data"
@@ -22,22 +19,14 @@ if ($hasNvimLuaCustomDirectory) {
     Remove-Item "$HOME\AppData\Local\nvim" -Recurse -Force
 }
 
-Write-Host "`nCloning NvChad" -ForegroundColor White
-git clone https://github.com/NvChad/NvChad $HOME\AppData\Local\nvim --depth 1
-
-
 Write-Host "`nMaking the symlinks" -ForegroundColor White
 Write-Output "2. Making the symlinks"
 
-$originPath = "$HOME\AppData\Local\nvim\lua\custom"
-$destinationPath = "$HOME\repos\dotfiles\.config\preconfigured-nvim\NvChad\custom\"
+$originPath = "$HOME\AppData\Local\nvim"
+$destinationPath = "$HOME\repos\dotfiles\.config\nvim"
 
 New-Item -ItemType Junction `
     -Path $originPath `
     -Target $destinationPath
 
 Write-Output "Job's done!"
-
-## other stuff
-# script to isntall kickstart nvim
-#
