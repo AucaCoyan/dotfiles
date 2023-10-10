605e48502bf6ae46244932be92fd81cea588389d
Auca Maillot <aucacoyan@gmail.com>
2023-10-10 07:46:09 -0300

    :sparkles: Add `jd` and `yt-dlp`

--- Windows/scripts/scoop-enternainment.ps1
+++ Windows/scripts/scoop-enternainment.ps1
@@ -6,5 +6,6 @@ scoop install bizhawk duckstation epic-games-launcher
 
 # scoop install jacket
 scoop install plex-server qbittorrent stremio
+scoop install jdownloader yt-dlp
 
 scoop install cyberduck
