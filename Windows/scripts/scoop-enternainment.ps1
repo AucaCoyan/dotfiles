ae5873e18501c345ee7dbad55a44fb9bb6708413
Auca Maillot <aucacoyan@gmail.com>
2023-11-06 11:22:32 -0300

    :heavy_plus_sign: Add `minecraft`

--- Windows/scripts/scoop-enternainment.ps1
+++ Windows/scripts/scoop-enternainment.ps1
@@ -3,6 +3,7 @@ scoop bucket add games
 
 # game emulator
 scoop install bizhawk duckstation epic-games-launcher
+scoop install prismlauncher # minecraft
 
 # scoop install jacket
 scoop install plex-server qbittorrent stremio
