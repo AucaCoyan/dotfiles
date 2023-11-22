ad4fa3e45dcd6983a5566804fff05a83d16a323e
Auca Maillot <aucacoyan@gmail.com>
2023-11-22 08:31:42 -0300

    :sparkles: Add `lazyvim` config

---
+++ .config/preconfigured-nvim/lazyvim/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- New tab
+-- keymap.set("n", "te", "tabedit") -- new tab
+keymap.set("n", "<tab>", ":bnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":bprev<Return>", opts)
