6878563f9565588de779b4bdf41464a6b1d85a36
Auca Coyan <aucacoyan@gmail.com>
2024-01-29 12:53:03 -0300

    :sparkles: add `repos` and git clones

--- Linux/Install-master-script.sh
+++ Linux/Install-master-script.sh
@@ -13,7 +13,7 @@ echo -e "###### Linux installation script ######"
 echo -e "\n### Updating the system"
 sudo apt-get update -y
 
-## `git` ##
+# `git`
 echo -e '\n### Installing Git..'
 sudo apt-get install git -y
 
@@ -28,12 +28,30 @@ echo -e 'Git has been configured!'
 echo -e '\n### `curl`'
 sudo apt install curl -y
 
-## Brew ##
+# Brew
 echo -e '\n### `brew`'
 # forward to /dev/null to bypass the "Press enter to continue" step
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
-# add brew to path
+## some brew dependencies / recommended pkgs
+sudo apt-get install build-essential
+brew install gcc
+## add brew to path
+echo -e '\n### Adding `brew` to PATH'
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# `nushell`
+brew install nushell
+
+mkdir ~/repos
+git clone https://github.com/AucaCoyan/dotfiles ~/repos/dotfiles
+
+mkdir ~/other-repos
+git clone https://github.com/nushell/nupm ~/other-repos/nupm
+git clone https://github.com/nushell/nu_scripts ~/other-repos/nu/nu_scripts
+
+ln -s ~/repos/dotfiles/nushell ~/.config/nushell
+
+brew install ripgrep
 
 ## install oh-my-posh
 echo -e '\n### `oh-my-posh`'
@@ -53,7 +71,9 @@ fc-cache
 
 # Rust
 echo -e '\n### Rust'
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo -e '\n### Reloading cargo env'
+source "$HOME/.cargo/env"
 
 # fnm (node & npm)
 echo -e '\n### `fnm`'
