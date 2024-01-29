72bed095614041b4ad07814b1ac385dc8bae1050
Auca Coyan <aucacoyan@gmail.com>
2024-01-29 12:01:30 -0300

    :bug: add `-e` flag to interpret newlines in echo

--- Linux/Install-master-script.sh
+++ Linux/Install-master-script.sh
@@ -7,40 +7,40 @@
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
-echo "###### Linux installation script ######"
+echo -e "###### Linux installation script ######"
 
 ## Update packages and Upgrade system
-echo "\nUpdating the system"
+echo -e "\nUpdating the system"
 sudo apt-get update -y
 
 ## `git` ##
-echo '\n###Installing Git..'
+echo -e '\n###Installing Git..'
 sudo apt-get install git -y
 
 # Git Configuration
-echo '\n ###Congigure Git..'
+echo -e '\n ###Congigure Git..'
 git config --global user.name "Auca Maillot"
 git config --global user.email "aucacoyan@gmail.com"
-echo 'Git has been configured!'
+echo -e 'Git has been configured!'
 # git config --list
 
 # `curl`
-echo '\n Installing `curl`'
+echo -e '\n Installing `curl`'
 sudo apt install curl -y
 
 ## Brew ##
-echo '\n ###Installing Brew'
+echo -e '\n ###Installing Brew'
 # forward to /dev/null to bypass the "Press enter to continue" step
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
 # add brew to path
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 ## install oh-my-posh
-echo '\n Installing `oh-my-posh`'
+echo -e '\n Installing `oh-my-posh`'
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 
 # install `FiraCode`
-echo '\n Installing Font FiraCode'
+echo -e '\n Installing Font FiraCode'
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz
@@ -48,13 +48,13 @@ curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCo
 tar xf FiraCode.tar.xz
 
 #reload the fonts
-echo '...reloading the fonts cache'
+echo -e '...reloading the fonts cache'
 fc-cache
 
 # Rust
-echo '\n ### Installing Rust'
+echo -e '\n ### Installing Rust'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # fnm (node & npm)
-echo '\n Installing `fnm`'
+echo -e '\n Installing `fnm`'
 curl -fsSL https://fnm.vercel.app/install | bash
