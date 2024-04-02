e0bdbd2a535aea124d9be560238398482b726cea
Auca Coyan <aucacoyan@gmail.com>
2024-04-02 20:18:51 -0300

    :sparkles: ps1 script for nvim-nu

---
+++ Windows/scripts/nvim-nu.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = "Stop"
+. "$PSScriptRoot\ps_support.ps1"
+
+
+Write-Output "Install nvim-nu in the machine"
+Write-Output "-----------------------------"
+
+# remove nvim-data
+$hasnNvimDataDirectory = Test-Path "$HOME\AppData\Local\nvim-data"
+if ($hasnNvimDataDirectory) {
+    Write-Host "`nRemoving nvim-data " -ForegroundColor White
+    Remove-Item "$HOME\AppData\Local\nvim-data" -Recurse -Force
+}
+
+# remove nvim
+$hasNvimLuaCustomDirectory = Test-Path "$HOME\AppData\Local\nvim"
+if ($hasNvimLuaCustomDirectory) {
+    Write-Host "`nRemoving nvim " -ForegroundColor White
+    Remove-Item "$HOME\AppData\Local\nvim" -Recurse -Force
+}
+
+Write-Host "`nMaking the symlinks" -ForegroundColor White
+Write-Output "2. Making the symlinks"
+
+$originPath = "$HOME\AppData\Local\nvim"
+$destinationPath = "$HOME\repos\dotfiles\.config\preconfigured-nvim\nvim-nu\"
+
+New-Item -ItemType Junction `
+    -Path $originPath `
+    -Target $destinationPath
+
+Write-Output "Job's done!"
