9e35e560c6c79667a29f498c8e022e28e6fbd29a
Auca Maillot <aucacoyan@gmail.com>
2024-06-08 11:19:31 -0300

    :adhesive_bandage: fix missing `=`

--- .config/preconfigured-nvim/kickstart.nvim/init.lua
+++ .config/preconfigured-nvim/kickstart.nvim/init.lua
@@ -87,7 +87,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.cmd.language 'en_US' -- forces vim to have en_US interface language
+vim.cmd.language = 'en_US' -- forces vim to have en_US interface language
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
