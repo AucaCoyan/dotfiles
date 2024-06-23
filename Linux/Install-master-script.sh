16a88f49cec07bed39242f66f3135dbf9bd78ebd
Auca Maillot <aucacoyan@gmail.com>
2024-06-23 09:39:01 -0300

    :wrench: set `Ctrl + Tab` to change tabs

--- Linux/Install-master-script.sh
+++ Linux/Install-master-script.sh
@@ -33,6 +33,9 @@ sudo apt install curl -y
 
 # Install catppuccin themes in Gnome Terminal
 curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.2.0/install.py | python3 -
+## Set Ctrl Tab to change tabs
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
 
 # `gh` CLI
 echo -e '\n### `gh`'
