a804b6de516ebbadea3c7bc43735905bf65e1dd0
Auca Maillot <aucacoyan@gmail.com>
2024-08-03 10:24:44 -0300

    :twisted_rightwards_arrows: merge

--- .config/preconfigured-nvim/kickstart.nvim/init.lua
+++ .config/preconfigured-nvim/kickstart.nvim/init.lua
@@ -697,7 +697,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        -- javascript = { { 'biome', 'prettierd', 'prettier' } },
         typescript = { 'biome' },
       },
     },
