33112242e2ec5463f1a85b2903ee9af0a1d62b50
Auca Coyan <aucacoyan@gmail.com>
2024-09-13 08:11:03 -0300

    :wrench: add format config for ts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -636,6 +636,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
