f5dcb3e3d1791185c026128914bb181b9f3f4291
Auca Maillot <aucacoyan@gmail.com>
2024-09-22 16:00:16 -0300

    :sparkles: add kitty shell

--- Linux/Install-master-script.sh
+++ Linux/Install-master-script.sh
@@ -37,6 +37,24 @@ curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.2.0/insta
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab'
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
 
+# Install Kitty Shell
+echo -e '\n### `Kitty shell`'
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n # do not launch kitty when finished
+
+echo -e '\n### `Desktop icon`'
+# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+# Update the paths to the kitty and its icon in the kitty desktop file(s)
+sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+
 # `gh` CLI
 echo -e '\n### `gh`'
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
