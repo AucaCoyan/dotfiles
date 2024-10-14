d4dfbb4dc44ac92d7b98fdcccac7b3d87d073031
Auca Coyan <aucacoyan@gmail.com>
2024-10-14 20:39:36 -0300

    :truck: move `autocmds` into its own file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -129,31 +129,6 @@ vim.keymap.set('n', '<leader>b', '<cmd>enew<CR>')
 -- buffer delete
 vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>', { desc = 'buffer delete' })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-vim.api.nvim_create_autocmd('BufWinEnter', {
-  desc = 'Help page in a new tab',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  pattern = { '*.txt' },
-  callback = function()
-    if vim.o.filetype == 'help' then
-      vim.cmd.wincmd 'T'
-    end
-  end,
-})
-
 -- inlay hints
 vim.lsp.inlay_hint.enable()
 vim.cmd.language = 'en_US' -- forces vim to have en_US interface language
@@ -192,6 +167,7 @@ vim.opt.sidescrolloff = 8
 
 --vim.g.python3_host_prog = 'C:/Users/AucaMaillo/repos/dotfiles/.config/.venv/Scripts/python.exe'
 
+require 'config.autocmd'
 -- structured installation
 -- https://lazy.folke.io/installation
 require 'config.lazy'
