8358c7701ffd60653b775d1ef73207ad1781d9c9
Auca Maillot <aucacoyan@gmail.com>
2024-11-24 10:46:31 -0300

    :sparkles: Add `thanos`

---
+++ Linux/nixOS/hosts/thanos/configuration.nix
@@ -0,0 +1,156 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  
+  nix.settings.experimental-features = ["nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
+  virtualisation.docker.logDriver = "json-file";
+
+  virtualisation.docker.daemon.settings = {
+    userland-proxy = false;
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Argentina/Buenos_Aires";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_AR.UTF-8";
+    LC_IDENTIFICATION = "es_AR.UTF-8";
+    LC_MEASUREMENT = "es_AR.UTF-8";
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";
+    LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";
+    LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "es_AR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  systemd.timers."shutdown-server" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Unit = "shutdown-server.service";
+    };
+  };
+
+systemd.services."shutdown-server" = {
+  script = ''
+    set -eu
+    /run/current-system/sw/bin/shutdown -h now
+  '';
+  serviceConfig = {
+    Type = "oneshot";
+    User = "root";
+  };
+};
+
+  # it works, but it's deprecated
+  # services.cron = {
+  #   enable = true;
+  #   systemCronJobs = [
+  #      "59 23 * * *      root       shutdown -h now" 
+  #   ];
+  # };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.thanos = {
+    isNormalUser = true;
+    description = "thanos";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+    shell = pkgs.nushell;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   bat
+   docker
+   docker-compose
+   gh
+   git
+   gitmoji-cli
+   grafana
+   grafana-loki
+   home-manager
+   gcc
+   oh-my-posh
+   postgresql
+   neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   nushell
+   wget
+  ];
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 5006 8080 9443 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
