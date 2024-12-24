0efb8305cf793d7ac2bd34a4dd893d213f8c4a55
Auca Coyan <aucacoyan@gmail.com>
2024-12-24 17:34:43 -0300

    :rewind: comment run single lua line

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- source the entire file
 vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+
 -- run the current line
-vim.keymap.set('n', '<space>x', ':.lua<CR>')
+-- vim.keymap.set('n', '<space>x', ':.lua<CR>')
+-- collides with delete buffer
+
 -- run the selected lines
 vim.keymap.set('v', '<space>x', ':lua<CR>')
