0103b8ab0663063bf44eaff551f14c50425b6ea5
Auca Coyan <aucacoyan@gmail.com>
2024-12-26 09:24:43 -0300

    :truck: move keymaps into the keymaps.lua file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -113,25 +113,6 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- [[ tabs]]
--- go to next tab
-vim.keymap.set('n', '<tab>', '<cmd>tabNext<CR>')
--- shift tab
-vim.keymap.set('n', '<S-tab>', '<cmd>tabprevious<CR>')
--- new tab
-vim.keymap.set('n', '<leader>b', '<cmd>enew<CR>', { desc = 'new [b]uffer' })
--- buffer delete
-vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>', { desc = '[b]uffer [d]elete' })
-
 vim.cmd.language = 'en_US' -- forces vim to have en_US interface language
 
 vim.opt.backup = false -- creates a backup file
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 -- source the entire file
-vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+-- vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+-- collides with find on opened buffers
 
 -- run the current line
 -- vim.keymap.set('n', '<space>x', ':.lua<CR>')
@@ -7,3 +8,22 @@ vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
 
 -- run the selected lines
 vim.keymap.set('v', '<space>x', ':lua<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- [[ tabs]]
+-- go to next tab
+vim.keymap.set('n', '<tab>', '<cmd>tabNext<CR>')
+-- shift tab
+vim.keymap.set('n', '<S-Tab>', '<cmd>tabprevious<CR>')
+-- new tab
+vim.keymap.set('n', '<leader>b', '<cmd>enew<CR>', { desc = 'new [b]uffer' })
+-- buffer delete
+vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>', { desc = '[b]uffer [d]elete' })
