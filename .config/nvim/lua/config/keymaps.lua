56cef5fc7d4c04784a7b9a223181652e2cc2e8cd
Auca Coyan <aucacoyan@gmail.com>
2025-01-02 11:56:41 -0300

    :sparkles: add a few keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -20,10 +20,27 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ tabs]]
 -- go to next tab
-vim.keymap.set('n', '<tab>', '<cmd>tabNext<CR>')
+vim.keymap.set('n', '<Tab>', '<cmd>tabNext<CR>')
 -- shift tab
 vim.keymap.set('n', '<S-Tab>', '<cmd>tabprevious<CR>')
 -- new tab
 vim.keymap.set('n', '<leader>b', '<cmd>enew<CR>', { desc = 'new [b]uffer' })
 -- buffer delete
 vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>', { desc = '[b]uffer [d]elete' })
+
+-- quickfix list
+vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>')
+
+-- save with Ctrl + S
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd> w <cr>')
+
+-- relative numbers
+-- vim.keymap.set("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
+-- vim.keymap.set("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+
+vim.keymap.set("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+
+-- set wrap
+vim.keymap.set({'n', 'i', 'v'}, '<A-z>', "<cmd>set wrap! <CR>", { desc = "Toggle word wrap"})
+
