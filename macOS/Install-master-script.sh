b7c7629236fadb6bbde1c38155a84f9eac159b1b
Auca Maillot <aucacoyan@gmail.com>
2025-01-04 16:40:23 -0300

    :sparkles: update more software

--- macOS/Install-master-script.sh
+++ macOS/Install-master-script.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/bash
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
@@ -16,8 +16,18 @@ brew install nushell
 brew install fzf
 brew install ripgrep
 
+brew install fnm
+fnm install --latest
+
+brew install --cask ghostty
+
+brew install --cask obsidian
+
 echo -e '\n### `oh-my-posh`'
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
+
+# install `FiraCode`
+brew install --cask font-fira-code-nerd-font
 
 # Git Configuration
 echo -e '\n### Configure git'
@@ -53,6 +63,10 @@ ln -s -F ~/repos/dotfiles/nushell ~/Library/Application\ Support/
 echo -e '\n### making a symlink to ~/repos/dotfiles/.config/nvim'
 ln -s -F ~/repos/dotfiles/.config/nvim ~/.config/nvim
 
+echo -e '\n### making a symlink to ~/repos/dotfiles/.config/ghostty'
+# mkdir -p ~/Library/Application\ Support/com.mitchellh.ghostty
+ln -s -F ~/repos/dotfiles/.config/ghostty ~/Library/Application\ Support/com.mitchellh.ghostty
+
 echo -e '\n### Rust'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
@@ -66,25 +80,3 @@ gitmoji init --default
 cargo binstall bat
 cargo binstall fd-find
 
-## add brew to path
-# echo -e '\n### Adding `brew` to PATH'
-# (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> "/home/$(whoami)/.bashrc"
-# ## add the env variables
-# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# echo -e '\n### reload .bashrc'
-# source ~/.bashrc
-
-# echo -e '\n### `fd`'
-# sudo apt-get install fd-find -y
-# echo -e '\n### symlinking fd-find to bat because of the name collition'
-# ln --symbolic --force --no-dereference $(which fdfind) ~/.local/bin/fd
-#
-#
-# # install `FiraCode`
-# echo -e '\nInstalling Font FiraCode'
-# mkdir --parents ~/.local/share/fonts
-# cd ~/.local/share/fonts
-# curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz
-# # unzip
-# tar xf FiraCode.tar.xz
