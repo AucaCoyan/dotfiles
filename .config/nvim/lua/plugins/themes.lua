ca005af3c345e291d9956df4fe7c92c4651c76f8
Auca Coyan <aucacoyan@gmail.com>
2025-03-20 11:02:03 -0300

    :lipstick: theme change

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,33 +1,35 @@
 return {
   {
     'folke/tokyonight.nvim',
-    enabled = false,
+    enabled = true,
   },
   { -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
+    enabled = false,
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     opts = {
-      flavour = 'latte',
+      flavour = 'mocha',
       transparent_background = true,
     },
+  },
+  {
+    'eldritch-theme/eldritch.nvim',
+    enabled = true,
+    lazy = false,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'eldritch'
 
       -- You can configure highlights by doing something like:
       --vim.cmd.hi 'Comment gui=none'
     end,
   },
   {
-    'eldritch-theme/eldritch.nvim',
-    lazy = false,
-  },
-  {
     'Mofiqul/vscode.nvim',
-    enabled = true,
+    enabled = false,
     opts = {
       transparent_background = true,
     },
