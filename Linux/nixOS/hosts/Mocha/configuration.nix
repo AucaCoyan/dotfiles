ad8f34edec62615eb39731f0276af09e1b4105f0
Auca Maillot <aucacoyan@gmail.com>
2025-04-25 08:44:17 -0300

    :sparkles: Add `nvd`

--- Linux/nixOS/hosts/Mocha/configuration.nix
+++ Linux/nixOS/hosts/Mocha/configuration.nix
@@ -144,6 +144,7 @@
    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    nh
    nushell
+   nvd # diff between nix versions
    obsidian
    oh-my-posh
    onedrive
