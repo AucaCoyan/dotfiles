57702482d4b7944d9ceefd7261d2223d60ffcbbf
Auca Maillot <aucacoyan@gmail.com>
2025-08-03 09:54:13 -0300

    :wrench: update the config of pre-commit

--- .config/preconfigured-nvim/NvChad/lua/configs/lspconfig.lua
+++ .config/preconfigured-nvim/NvChad/lua/configs/lspconfig.lua
@@ -3,4 +3,4 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls" , "biome", "basedpyright", "rust_analyzer"}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
