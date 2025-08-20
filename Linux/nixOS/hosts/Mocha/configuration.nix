9d13662f88e3e7826a93886412966e4e71aab7fb
Auca Maillot <aucacoyan@gmail.com>
2025-08-20 19:09:08 -0300

    :construction: Wip samba

--- Linux/nixOS/hosts/Mocha/configuration.nix
+++ Linux/nixOS/hosts/Mocha/configuration.nix
@@ -34,6 +34,18 @@
     "flakes"
   ];
 
+  # fileSystems."/mnt/share" = {
+  #   device = "//192.168.0.8/";
+  #   fsType = "cifs";
+  #   options =
+  #     let
+  #       # this line prevents hanging on network split
+  #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+
+  #     in
+  #     [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
+  # };
+
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
@@ -206,6 +218,7 @@
     bat
     bun
     cargo-tarpaulin # rust coverage
+    cifs-utils # for the samba shares
     delta
     difftastic
     discord
@@ -251,6 +264,7 @@
     # postgresql
     ripgrep
     rustup
+    samba
     signal-desktop
     telegram-desktop
     ticktick
