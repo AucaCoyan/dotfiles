4a1bc4734e75522d7d86e673b32e143c23ede9c2
Auca Maillot <aucacoyan@gmail.com>
2025-09-18 08:40:35 -0300

    :lipstick: change the theme for gruvbox

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,11 +1,11 @@
 return {
   {
     'folke/tokyonight.nvim',
-    enabled = true,
+    enabled = false,
   },
   { -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
-    enabled = true,
+    enabled = false,
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     opts = {
@@ -13,15 +13,6 @@ return {
       transparent_background = true,
     },
 
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
-
-      -- You can configure highlights by doing something like:
-      --vim.cmd.hi 'Comment gui=none'
-    end,
   },
   {
     'eldritch-theme/eldritch.nvim',
@@ -35,5 +26,16 @@ return {
       transparent_background = true,
     },
   },
-  { 'ellisonleao/gruvbox.nvim', enabled = true, priority = 1000, opts = { transparent_background = true } },
+  { 'ellisonleao/gruvbox.nvim', enabled = true, priority = 1000, opts = { transparent_background = true },
+
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'gruvbox'
+
+      -- You can configure highlights by doing something like:
+      --vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
