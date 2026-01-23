4de76cec20befa52579eca359213099af484444b
Auca Maillot <aucacoyan@gmail.com>
2026-01-23 07:55:52 -0300

    :sparkles: add `ts_ls` lsp

--- .config/preconfigured-nvim/kickstart.nvim/init.lua
+++ .config/preconfigured-nvim/kickstart.nvim/init.lua
@@ -690,7 +690,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
